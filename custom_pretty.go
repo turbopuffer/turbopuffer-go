d112cc11e286f7c430a12e1428e8fb102d2d87c0
Elias Tran <trandinhphuc1997@gmail.com>
2026-04-21 23:04:07 -0400

    fix: pretty print error messages (#107)
    Rather than printing an error about failing to JSON marshal the errors.

--- custom_pretty.go
+++ custom_pretty.go
@@ -9,6 +9,9 @@ import (
 
 // PrettyPrint pretty prints a JSON-encodable value to a string.
 func PrettyPrint(v any) string {
+	if err, ok := v.(error); ok {
+		return fmt.Sprintf("<error: %s>", err.Error())
+	}
 	bytes, err := json.Marshal(v)
 	if err != nil {
 		return fmt.Sprintf("<error: %s>", err)
