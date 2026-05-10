46b9fdfaea0863110e03f5b1055361c9f7b91f7c
Nikhil Benesch <nikhil.benesch@gmail.com>
2026-05-10 14:01:06 -0400

    feat(go): add QueryParam alias for NamespaceMultiQueryParamsQuery

---
+++ custom_type_aliases.go
@@ -0,0 +1,5 @@
+package turbopuffer
+
+// QueryParam is an alias for [NamespaceMultiQueryParamsQuery], which describes
+// a single query in a multi-query request.
+type QueryParam = NamespaceMultiQueryParamsQuery
